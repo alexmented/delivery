b932385f472516d0772643bcb902658d4c04e528
Sherstnev Aleksey <sm_alex99@mail.ruu>
2025-11-16 22:17:17 +0100

    add Location Value Object

--- src/main/java/delivery/DeliveryApplication.java
+++ src/main/java/delivery/DeliveryApplication.java
@@ -2,11 +2,12 @@ package delivery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import delivery.core.domain.model.Location;
 
 @SpringBootApplication
 public class DeliveryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DeliveryApplication.class, args);
-	}
+        SpringApplication.run(DeliveryApplication.class, args);
+    }
 }
---
+++ src/test/java/delivery/core/domain/model/LocationTest.java
@@ -0,0 +1,68 @@
+package delivery.core.domain.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import static org.assertj.core.api.Assertions.assertThat;
+
+class LocationTest {
+
+    @Test
+    void testCreateWithValidCoordinates() {
+        var result = Location.create(5, 7);
+        assertThat(result.isSuccess()).isTrue();
+        assertThat(result.getValue().getX()).isEqualTo(5);
+        assertThat(result.getValue().getY()).isEqualTo(7);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -10, 11, 15, 100})
+    void testCreateWithInvalidX(int invalidX) {
+        var result = Location.create(invalidX, 5);
+        assertThat(result.isFailure()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, -1, -10, 11, 15, 100})
+    void testCreateWithInvalidY(int invalidY) {
+        var result = Location.create(5, invalidY);
+        assertThat(result.isFailure()).isTrue();
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 5, 8, 10})
+    void testCreateWithValidCoordinates(int coordinate) {
+        var result = Location.create(coordinate, coordinate);
+        assertThat(result.isSuccess()).isTrue();
+    }
+
+    @Test
+    void testCreateRandom() {
+        var location = Location.createRandom();
+        assertThat(location).isNotNull();
+        assertThat(location.getX()).isBetween(1, 10);
+        assertThat(location.getY()).isBetween(1, 10);
+    }
+
+    @Test
+    void testDistanceTo() {
+        var location1 = Location.create(2, 3).getValue();
+        var location2 = Location.create(5, 7).getValue();
+        var result = location1.distanceTo(location2);
+        assertThat(result.getValue()).isEqualTo(7);
+    }
+
+    @Test
+    void testDistanceToSameLocation() {
+        var location = Location.create(5, 5).getValue();
+        var result = location.distanceTo(location);
+        assertThat(result.getValue()).isEqualTo(0);
+    }
+
+    @Test
+    void testEquality() {
+        var location1 = Location.create(5, 7).getValue();
+        var location2 = Location.create(5, 7).getValue();
+        assertThat(location1).isEqualTo(location2);
+    }
+}
