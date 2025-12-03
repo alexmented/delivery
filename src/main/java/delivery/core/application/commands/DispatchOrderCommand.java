9fdbfa424685c826ca817ec6cb8a66e470168b45
Aleksei Sherstnev <sm_alex99@mail.r>
2025-12-03 17:44:51 +0100

    module 6

--- src/main/java/delivery/core/application/commands/DispatchOrderCommand.java
+++
@@ -1,6 +0,0 @@
-package delivery.core.application.commands;
-
-import java.util.UUID;
-
-public record DispatchOrderCommand(UUID orderId) {
-}
--- src/main/java/delivery/core/application/commands/DispatchOrderCommandHandler.java
+++
@@ -1,8 +0,0 @@
-package delivery.core.application.commands;
-
-import libs.errs.Error;
-import libs.errs.UnitResult;
-
-public interface DispatchOrderCommandHandler {
-    UnitResult<Error> handle(DispatchOrderCommand command);
-}
